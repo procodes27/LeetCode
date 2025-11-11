f5a52a61f4e9b9d7e267429b94bff48be0786b24
PRINCE KUMAR <sukheprince27@gmail.com>
2025-11-11 21:00:34 +0530

    Time: 137 ms (57.79%), Space: 39.7 MB (54.11%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int t[101][101][601];
+    int solve(vector<pair<int, int>>& count, int m, int n, int index) {
+        if(index >= count.size() || (m == 0 && n == 0))
+            return 0;
+        if(t[m][n][index] != -1)
+            return t[m][n][index];
+        
+        int include = 0;
+        if(count[index].first <= m && count[index].second <= n) {
+            include = 1 + solve(count, m-count[index].first, n-count[index].second, index+1);
+        }
+        
+        int exclude = solve(count, m, n, index+1);
+        
+        return t[m][n][index] = max(include, exclude);
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int N = strs.size();
+        vector<pair<int, int>> count(N);
+        int i = 0;\
+        for(string str:strs) {
+            int countOnes  = 0;
+            int countZeros = 0;
+            
+            for(char ch:str) {
+                if(ch == '1')
+                    countOnes++;
+                else
+                    countZeros++;
+            }
+            
+            count[i++] = {countZeros, countOnes};
+        }
+        
+        memset(t, -1, sizeof(t));
+        return solve(count, m, n, 0);
+    }
+};
