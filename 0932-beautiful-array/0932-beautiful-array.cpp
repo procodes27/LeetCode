8e34327eb67f0cb4117101f943ecb5e749c93f27
PRINCE KUMAR <sukheprince27@gmail.com>
2026-02-03 00:40:03 +0530

    Time: 0 ms (100%), Space: 10.2 MB (45.03%) - LeetHub

---
+++ 0932-beautiful-array/0932-beautiful-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> beautifulArray(int n) {
+        vector<int> res = {1};
+
+        while (res.size() < n) {
+            vector<int> temp;
+
+            for (int x : res)
+                if (2 * x - 1 <= n)
+                    temp.push_back(2 * x - 1);
+
+            for (int x : res)
+                if (2 * x <= n)
+                    temp.push_back(2 * x);
+
+            res = temp;
+        }
+        return res;
+    }
+};
